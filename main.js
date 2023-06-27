09f8f0711d4d9c1e48698f8d56c433df96de01af
Leydis <leydisfh@gmail.com>
2023-06-27 11:37:17 -0500

    Clase02 Completada

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const btn = document.querySelector("[data-form-btn]");
-const  input = document.querySelector("[data-form-input]")
+
 
 const createTask = (event) =>{
     event.preventDefault();
-    console.log(input.value)
+    const  input = document.querySelector("[data-form-input]");
+    const inputValue = input.value;
+    const cardsList = document.querySelector("[data-cardList]")
+    const task = document.createElement("li");
+    task.classList.add("card")
+    
+    input.value = "";
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${inputValue}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    cardsList.appendChild(task);
 }
 
+btn.addEventListener("click", createTask);
 
-btn.addEventListener("click", createTask)
 
 
